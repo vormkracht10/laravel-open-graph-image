1e2724237aa6c689d805cc43ece0001e5a392462
Bas van Dinther <bas@vormkracht10.nl>
2022-08-19 10:47:55 +0200

    Uncomment routes

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,4 @@ use Illuminate\Support\Facades\Route;
 use Vormkracht10\LaravelOpenGraphImage\Http\Controllers\LaravelOpenGraphImageController;
 
 Route::get('open-graph-image', [LaravelOpenGraphImageController::class, '__invoke'])->name('open-graph-image');
-
-// Route::get('open-graph-image', [LaravelOpenGraphImageController::class])->name('open-graph-image');
-// Route::get('open-graph-image.jpg', [LaravelOpenGraphController::class])->name('open-graph-image-file');
+Route::get('open-graph-image.jpg', [LaravelOpenGraphController::class, '__invoke'])->name('open-graph-image-file');
