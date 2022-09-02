6c6ae1faf20cb4d70ecf7f71a3ba1965c8214f45
Bas van Dinther <bas@vormkracht10.nl>
2022-09-02 10:54:49 +0200

    Pass subtitle and title to component

--- resources/views/components/metatags.blade.php
+++ resources/views/components/metatags.blade.php
@@ -1,1 +1,1 @@
-<meta property="og:image" content="{!! url()->signedRoute('open-graph-image', ['title' => 'Lorem ipsum', 'subtitle' => 'dolor sit amet']) !!}">
+<meta property="og:image" content="{!! url()->signedRoute('open-graph-image', ['title' => $title, 'subtitle' => $subtitle]) !!}">
--- src/Components/Metatags.php
+++ src/Components/Metatags.php
@@ -6,13 +6,21 @@ use Illuminate\View\Component;
 
 class Metatags extends Component
 {
-    /**
-     * Get the view / contents that represent the component.
-     *
-     * @return \Illuminate\Contracts\View\View|\Closure|string
-     */
+
+    public $title;
+    public $subtitle;
+
+    public function __construct(string $title, string $subtitle)
+    {
+        $this->title = $title;
+        $this->subtitle = $subtitle;
+    }
+
     public function render()
     {
-        return view('open-graph-image::components.metatags');
+        return view('open-graph-image::components.metatags', [
+            'title' => $this->title,
+            'subtitle' => $this->subtitle,
+        ]);
     }
 }
