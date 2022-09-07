e5c92ecc7fb36d64859c475a2d30f245b55a68d1
Bas van Dinther <bas@vormkracht10.nl>
2022-09-07 10:47:14 +0200

    Add isset check to subtitle

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -41,7 +41,7 @@
     <body class="flex items-center justify-center min-h-screen">
         <div class="bg-gray-100 w-[1200px] h-[630px] text-white p-12 border-rose-500 border-[16px] rounded-lg">
             <h1 class="font-bold text-[90px] text-rose-500 leading-none">{!! explode(' - ', $title)[0] !!}</h1>
-            @if($subtitle)
+            @if(isset($subtitle))
                 <h2 class="mt-6 text-[50px] font-bold text-gray-400 uppercase">{{ $subtitle }}</h2>
             @endif
             <div class="inline-block px-6 py-3 mt-10 text-[30px] font-bold text-white rounded-lg bg-rose-500">Read more here</div>
