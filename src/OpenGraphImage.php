e95baa4067621a569130148f95609fb0cdff8698
Mark van Eijk <mark@vormkracht10.nl>
2023-01-21 13:31:10 +0100

    Update OpenGraphImage.php

--- src/OpenGraphImage.php
+++ src/OpenGraphImage.php
@@ -4,8 +4,13 @@ namespace Vormkracht10\LaravelOpenGraphImage;
 
 class OpenGraphImage
 {
-    public function url(...$args)
+    public function url(...$parameters)
     {
-        return url()->signedRoute('open-graph-image.file', ...$args);
+        $parameters = collect($parameters);
+            ->merge(['.'.config('open-graph-image.image.extension')]) // add image extension to url for twitter compatibility
+            ->all();
+
+        return url()
+            ->signedRoute('open-graph-image.file', $parameters);
     }
 }
