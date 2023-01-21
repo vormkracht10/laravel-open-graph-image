9a4af4fa8e5d00ea3903417fd48bc42c29262685
Mark van Eijk <mark@vormkracht10.nl>
2023-01-21 14:05:20 +0100

    make sure parameters given as ComponentAttributeBag converts to an array

--- src/OpenGraphImage.php
+++ src/OpenGraphImage.php
@@ -2,10 +2,21 @@
 
 namespace Vormkracht10\LaravelOpenGraphImage;
 
+use Illuminate\View\ComponentAttributeBag;
+
 class OpenGraphImage
 {
-    public function url(...$parameters)
+    public function transformAttributeBagToArray(ComponentAttributeBag $attributes): array
     {
+        return collect($attributes)->all();
+    }
+
+    public function url(array|ComponentAttributeBag $parameters): string
+    {
+        if($parameters instanceof ComponentAttributeBag) {
+            $parameters = $this->transformAttributeBagToArray($parameters);
+        }
+
         $parameters = collect($parameters)
             ->merge(['.'.config('open-graph-image.image.extension')]) // add image extension to url for twitter compatibility
             ->all();
